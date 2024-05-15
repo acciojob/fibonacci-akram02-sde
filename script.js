20a49863bdfea40083d750324b08363872f55e44
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-05-15 11:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function fibonacci(num) {
 // your code here
-	if(num == 1){
-		return 0
+	let num1 = 0
+	let num2 = 1
+	let sum = 0
+	if (num == 1){
+		return num1
 	}
-	if(num == 2){
-		return 1
+	else if(num == 2){
+		return num2
 	}
-	return fibonacci(num - 1) + fibonacci(num - 2)
+	else {
+		for (let i = 3; i <= num; i++) {
+			sum = num1 + num2
+			num1 = num2
+			num2 = sum
+		}
+		return num2
+	}
 }
 
 module.exports = fibonacci;
